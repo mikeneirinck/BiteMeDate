f4c4f38c9db3b66d8b2a8c3331b7e0a6b0ff8be3
mikeneirinck <mikeneirinck@Mikes-Mac-mini.local>
2015-05-02 19:13:13 +0200

    Initial Commit

---
+++ BiteMeDate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BiteMeDate
+//
+//  Created by Mike Neirinck on 2/05/15.
+//  Copyright (c) 2015 mike.neirinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
